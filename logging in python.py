1ba47b7b6029fc20a080baf57b1695b2a5776e37
godalming123 <68993177+godalming123@users.noreply.github.com>
2020-09-20 11:52:54 +0100

    make python file funier

--- logging in python.py
+++ logging in python.py
@@ -5,9 +5,9 @@ logging.basicConfig (filename = "logger.log", level="DEBUG")
 
 logger = logging.getLogger ()
 
-logger.debug ("debug")
-logger.info ("info")
-logger.warning ("warning")
+logger.debug ("why did I tell the logger to warn me about telling the logger to warn me")
+logger.info ("my logger file")
+logger.warning ("why did you put warning here, also why did you put an error on line 11 and a critical on line 12")
 logger.error ("error")
 logger.critical ("critical")
 
