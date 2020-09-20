bd679a762120cf14e6101bd775f1216f1cd5d77f
godalming123 <68993177+godalming123@users.noreply.github.com>
2020-09-20 11:53:59 +0100

    update logger to make more sense

--- logging in python.py
+++ logging in python.py
@@ -5,8 +5,8 @@ logging.basicConfig (filename = "logger.log", level="DEBUG")
 
 logger = logging.getLogger ()
 
-logger.debug ("why did I tell the logger to warn me about telling the logger to warn me")
 logger.info ("my logger file")
+logger.debug ("why did I tell the logger to warn me about telling the logger to warn me")
 logger.warning ("why did you put warning here, also why did you put an error on line 11 and a critical on line 12")
 logger.error ("error")
 logger.critical ("critical")
