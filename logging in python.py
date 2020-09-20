27df0dcde0c5a41f9e4079ef814ce496b43a19f4
godalming123 <68993177+godalming123@users.noreply.github.com>
2020-09-20 11:26:58 +0100

    reformat python file

--- logging in python.py
+++ logging in python.py
@@ -2,13 +2,16 @@ from logging import *
 LoggerFormat = "%(Levelname)s %(asctime)s - %(message)s"
 basicConfig (filename = "logger.log", level=DEBUG, format = LoggerFormat)
 logger = getLogger ()
+
 logger.debug ("debug")
 logger.info ("info")
 logger.warning ("warning")
 logger.error ("error")
 logger.critical ("critical")
+
 try :
   #code
   pass
+
 except error as e:
   logger.error (e)
