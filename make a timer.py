e0c04ec484f81d28d7d93b0f5dbb438df5d8646d
godalming123 <68993177+godalming123@users.noreply.github.com>
2020-11-28 09:56:10 +0000

    it works

--- make a timer.py
+++ make a timer.py
@@ -1,27 +1,28 @@
-from datetime import *
+from time import *
 while True :
-    running = True
-    sec = input ("seconds: ")
-    minutes = input ("minutes: ")
-    hours = input ("hours: ")
-    date = date ()
-    try :
-        sec = int (sec)
-        minutes  = int (minutes)
-        hours = int (hours)
-    except :
-        print (hours, ", ", sec, "or", minutes, " is not a valid number")
-        running = False
-    while running :
-        print (hours, ":", minutes, ":", sec)
-        sleep (1)
-        if sec < 0 and minutes < 0 and hours < 0:
-            print ("timer done")
-            running = False
-        elif minutes < 0 and sec < 0:
-            hours -= 1
-            minutes = 60
-        elif sec < 0 : 
-            minutes -= 1
-            sec = 60
-        sec -= 1
+		running = True
+		sec = input ("seconds: ")
+		minutes = input ("minutes: ")
+		hours = input ("hours: ")
+
+		try :
+				sec = int (sec)
+				minutes	= int (minutes)
+				hours = int (hours)
+		except :
+				print (hours, ", ", sec, "or", minutes, " is not a valid number")
+				running = False
+		
+		while running :
+				print (hours, ":", minutes, ":", sec)
+				sleep (1)
+
+				if sec > 0 :
+					sec -= 1
+				elif minutes > 0:
+					minutes -= 1
+					sec = 59
+				elif hours > 0:
+					hours -= 1
+					minutes = 59
+					sec = 59
