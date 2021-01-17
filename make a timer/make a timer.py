3decac541d88914c96359c9b52e34a4eb7832bb8
godalming123 <68993177+godalming123@users.noreply.github.com>
2021-01-17 10:44:22 +0000

    reformat

--- make a timer/make a timer.py
+++ make a timer/make a timer.py
@@ -1,28 +1,30 @@
 from time import *
 while True :
-		running = True
-		sec = input ("seconds: ")
-		minutes = input ("minutes: ")
-		hours = input ("hours: ")
+	running = True
+	sec = input ("seconds: ")
+	minutes = input ("minutes: ")
+	hours = input ("hours: ")
 
-		try :
-				sec = int (sec)
-				minutes	= int (minutes)
-				hours = int (hours)
-		except :
-				print (hours, ", ", sec, "or", minutes, " is not a valid number")
-				running = False
+	try :
+		sec = int (sec)
+		minutes	= int (minutes)
+		hours = int (hours)
+	except :
+		print (hours, ", ", sec, "or", minutes, " is not a valid number")
+		running = False
 		
-		while running :
-				print (hours, ":", minutes, ":", sec)
-				sleep (1)
+	while running :
+		print (hours, ":", minutes, ":", sec)
+		sleep (1)
 
-				if sec > 0 :
-					sec -= 1
-				elif minutes > 0:
-					minutes -= 1
-					sec = 59
-				elif hours > 0:
-					hours -= 1
-					minutes = 59
-					sec = 59
+		if sec > 0 :
+			sec -= 1
+
+		elif minutes > 0:
+			minutes -= 1
+			sec = 59
+			
+		elif hours > 0:
+			hours -= 1
+			minutes = 59
+			sec = 59
